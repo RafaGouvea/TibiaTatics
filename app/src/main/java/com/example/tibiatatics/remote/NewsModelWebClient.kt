c6cc0deb58ffc50e0f71fd6be05304f97ffbae97
Rafael <rafa.8404@hotmail.com>
2023-08-09 02:27:19 -0300

    added home layout

--- app/src/main/java/com/example/tibiatatics/remote/NewsModelWebClient.kt
+++ app/src/main/java/com/example/tibiatatics/remote/NewsModelWebClient.kt
@@ -2,24 +2,21 @@ package com.example.tibiatatics.remote
 
 import android.util.Log
 import com.example.tibiatatics.model.NewsModel
-import com.example.tibiatatics.model.NewsListResponse
 import com.example.tibiatatics.model.toModel
-import com.example.tibiatatics.ui.adapter.HomeFragmentAdapter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class NewsModelWebClient() {
 
     private val apiInterface: ApiNews = RetrofitSetup.createService(ApiNews::class.java)
 
     suspend fun loadFrom(): List<NewsModel>? {
-        val resposta = apiInterface.getNewsLasted()
-        return resposta.body()?.news?.map {
-            it.toModel()
+       return try {
+            val resposta = apiInterface.getNewsLasted()
+            resposta.body()?.news?.map {
+                it.toModel()
+            }
+        } catch (e: Exception) {
+           Log.e("###", "loadFrom: ", e)
+            null
         }
     }
 }
