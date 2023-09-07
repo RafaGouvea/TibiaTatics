02d7f86ce34756807f65e47b20fabf281a637b89
Rafael <rafa.8404@hotmail.com>
2023-09-06 21:46:24 -0300

    trying to fix dmg skill formula

--- app/src/main/java/com/example/tibiatatics/ui/fragments/KnightCalcFragment.kt
+++ app/src/main/java/com/example/tibiatatics/ui/fragments/KnightCalcFragment.kt
@@ -16,7 +16,7 @@ import java.text.DecimalFormat
 class KnightCalcFragment : Fragment() {
 
     private var attackMode = 1.0
-    private var dmgType = 0
+    private var dmgType = 1
 
 
     override fun onCreateView(
@@ -49,7 +49,8 @@ class KnightCalcFragment : Fragment() {
         val elementalAttackInt = elementalAttack.text.toString().toIntOrNull() ?: 0
 
         val physicalAttack = view.findViewById<TextInputEditText>(R.id.input_physical_attack)
-        val physicalAttackInt = physicalAttack.text.toString().toIntOrNull() ?: 0
+        var physicalAttackInt = physicalAttack.text.toString().toIntOrNull() ?: 1
+        if (physicalAttackInt == 0) { physicalAttackInt = 1 }
 
         val physicalResistance =
             view.findViewById<TextInputEditText>(R.id.input_creature_physical_resistence)
@@ -75,7 +76,8 @@ class KnightCalcFragment : Fragment() {
         val deathResistanceInt = deathResistance.text.toString().toIntOrNull() ?: 100
 
         val armor = view.findViewById<TextInputEditText>(R.id.input_creature_armor)
-        val armorInt = armor.text.toString().toIntOrNull() ?: 1
+        var armorInt = armor.text.toString().toIntOrNull() ?: 1
+        if (armorInt > 130) { armorInt = 130 }
 
         val resistanceElemental = when (dmgType) {
             1 -> earthResistanceInt
@@ -94,60 +96,90 @@ class KnightCalcFragment : Fragment() {
 
         // CALCULO DE DANO CORPO A CORPO
         val calcTotalDmgPhysical =
-            (0.085 * attackMode * physicalAttackInt * skillsInt) + (levelInt / 5)
-        val calcTotalDmgElemental =
-            (0.085 * attackMode * elementalAttackInt * skillsInt) + (levelInt / 5) - ((levelInt / 100) * 20)
+            (0.085 * attackMode * physicalAttackInt * skillsInt) + (levelInt / 5).toDouble()
+        val calcTotalDmgElemental = 0.085 * attackMode * elementalAttackInt * skillsInt
 
         val d = calcTotalDmgPhysical - calcArmor
-        val dmgWithArmor = if (d <= 0 || physicalAttackInt == 0) 0 else d.toInt()
-
+        val dmgWithArmor = if (d <= 0) 0 else d.toInt()
         val dmgPhysicalWithResistence = dmgWithArmor * physicalResistanceInt / 100
-        val dmgElementalWithResistence = calcTotalDmgElemental * resistanceElemental / 100
+
+        val dmgElementalWithResistence = (calcTotalDmgElemental * resistanceElemental) / 100
         val dmgTotal = dmgElementalWithResistence + dmgPhysicalWithResistence
 
 
         // CALCULO DE DANO MINIMO EXORI MAS
         val dmgMinExoriMas =
-            0.5 * (skillsInt + physicalAttackInt + elementalAttackInt) + (levelInt / 5)
+            0.5 * (skillsInt + physicalAttackInt + elementalAttackInt) + (levelInt / 5).toDouble()
         //total de dano FISICO minimo exori mas
         val minPhysicalDmgExoriMas = (dmgMinExoriMas / 100) * percentagePhysicalAttack
         val dmgMinExoriMasWithArmor = minPhysicalDmgExoriMas - calcArmor
         val dmgMinPhysicalExoriMasTotal =
-            if (dmgMinExoriMasWithArmor <= 0
-                || physicalAttackInt == 0
-            ) 0 else
+            if (dmgMinExoriMasWithArmor <= 0) 0 else
                 dmgMinExoriMasWithArmor.toInt() * physicalResistanceInt / 100
         //Total de dano ELEMENTAL minimo exori mas
         val minElementalDmgExoriMas = (dmgMinExoriMas / 100) * percentageElementalAttack
         val dmgElementalExoriMasMinTotal = minElementalDmgExoriMas * resistanceElemental / 100
-        val dmgMinElementalExoriMas =
-            if (elementalAttackInt == 0 && resistanceElemental == 0) 0 else dmgElementalExoriMasMinTotal.toInt()
         //Total de dano minimo exori mas
-        val minExoriMasTotalDmg = dmgMinElementalExoriMas + dmgMinPhysicalExoriMasTotal
+        val minExoriMasTotalDmg = dmgElementalExoriMasMinTotal + dmgMinPhysicalExoriMasTotal
 
         // CALCULO DE DANO MAXIMO EXORI MAS
         val dmgMaxExoriMas =
-            1.1 * (skillsInt + physicalAttackInt + elementalAttackInt) + (levelInt / 5)
+            1.1 * (skillsInt + physicalAttackInt + elementalAttackInt) + (levelInt / 5).toDouble()
         //total de dano FISICO maximo exori mas
         val maxPhysicalDmgExoriMas = (dmgMaxExoriMas / 100) * percentagePhysicalAttack
         val dmgMaxExoriMasWithArmor = maxPhysicalDmgExoriMas - calcArmor
         val dmgMaxPhysicalExoriMasTotal =
-            if (dmgMaxExoriMasWithArmor <= 0
-                || physicalAttackInt == 0
-            ) 0 else
+            if (dmgMaxExoriMasWithArmor <= 0) 0 else
                 dmgMaxExoriMasWithArmor.toInt() * physicalResistanceInt / 100
         //Total de Dano ELEMENTAL maximo exori mas
         val maxElementalDmgExoriMas = (dmgMaxExoriMas / 100) * percentageElementalAttack
         val dmgElementalExoriMasMaxTotal = maxElementalDmgExoriMas * resistanceElemental / 100
-        val dmgMaxElementalExoriMas =
-            if (elementalAttackInt == 0 && resistanceElemental == 0) 0 else dmgElementalExoriMasMaxTotal.toInt()
         //Total de dano maximo Exori Mas
-        val maxExoriMasTotalDmg = dmgMaxElementalExoriMas + dmgMaxPhysicalExoriMasTotal
+        val maxExoriMasTotalDmg = dmgElementalExoriMasMaxTotal + dmgMaxPhysicalExoriMasTotal
 
-        val averageMinDmgExoriMas = (minExoriMasTotalDmg + maxExoriMasTotalDmg) / 2
+        val averageDmgExoriMas = (minExoriMasTotalDmg + maxExoriMasTotalDmg) / 2
+
+
+
+        // CALCULO DE DANO MINIMO EXORI GRAN
+        val dmgMinExoriGran =
+            ((skillsInt + physicalAttackInt + elementalAttackInt) * 2) * 1.1 + (levelInt / 5).toDouble()
+        //total de dano FISICO minimo exori gran
+        val minPhysicalDmgExoriGran = (dmgMinExoriGran / 100) * percentagePhysicalAttack
+        val dmgMinExoriGranWithArmor = minPhysicalDmgExoriGran - calcArmor
+        val dmgMinPhysicalExoriGranTotal =
+            if (dmgMinExoriGranWithArmor <= 0) 0 else
+                dmgMinExoriGranWithArmor.toInt() * physicalResistanceInt / 100
+        //Total de dano ELEMENTAL minimo exori gran
+        val minElementalDmgExoriGran = (dmgMinExoriGran / 100) * percentageElementalAttack
+        val dmgElementalExoriGranMinTotal = minElementalDmgExoriGran * resistanceElemental / 100
+        //Total de dano minimo exori gran
+        val minExoriGranTotalDmg = dmgElementalExoriGranMinTotal + dmgMinPhysicalExoriGranTotal
+
+
+        // CALCULO DE DANO MAXIMO EXORI GRAN
+        val dmgMaxExoriGran =
+            ((skillsInt + physicalAttackInt + elementalAttackInt) * 2) * 3 + (levelInt / 5).toDouble()
+        //total de dano FISICO minimo exori gran
+        val maxPhysicalDmgExoriGran = (dmgMaxExoriGran / 100) * percentagePhysicalAttack
+        val dmgMaxExoriGranWithArmor = maxPhysicalDmgExoriGran - (calcArmor)
+        val dmgMaxPhysicalExoriGranTotal =
+            if (dmgMaxExoriGranWithArmor <= 0) 0 else
+                dmgMaxExoriGranWithArmor.toInt() * physicalResistanceInt / 100
+        //Total de dano ELEMENTAL minimo exori gran
+        val maxElementalDmgExoriGran = (dmgMaxExoriGran / 100) * percentageElementalAttack
+        val dmgElementalExoriGranMaxTotal = maxElementalDmgExoriGran * resistanceElemental / 100
+        //Total de dano minimo exori gran
+        val maxExoriGranTotalDmg = dmgElementalExoriGranMaxTotal + dmgMaxPhysicalExoriGranTotal
+
+
+        val averageExoriGran = (maxExoriGranTotalDmg + minExoriGranTotalDmg) / 2
+
+
+
 
         val formato = DecimalFormat("#")
-        return formato.format(averageMinDmgExoriMas)
+        return formato.format(dmgMinExoriGran)
     }
 
     private fun dropMenuAttackMode(view: View) {
@@ -188,8 +220,7 @@ class KnightCalcFragment : Fragment() {
                 "Fire Damage",
                 "Death Damage",
                 "Energy Damage",
-                "Ice Damage",
-                "None"
+                "Ice Damage"
             )
 
 
@@ -200,7 +231,7 @@ class KnightCalcFragment : Fragment() {
         )
         actElementalType.setAdapter(elementalTypeAdapter)
 
-        actElementalType.setText("None", false)
+        actElementalType.setText("Earth Damage", false)
         actElementalType.setOnClickListener {
             actElementalType.showDropDown()
         }
